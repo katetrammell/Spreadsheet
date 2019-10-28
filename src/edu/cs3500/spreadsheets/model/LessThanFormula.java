7dfe1a1caba97b19f4831724f1184f8da08b1054
Ian McLaughlin <mclaughlin.i@husky.neu.edu>
2019-10-28 16:45:45 -0400

    added file

---
+++ src/edu/cs3500/spreadsheets/model/LessThanFormula.java
@@ -0,0 +1,40 @@
+package edu.cs3500.spreadsheets.model;
+
+/**
+ * Class that represent lessThanFormula.
+ */
+public class LessThanFormula implements Formula<Boolean> {
+
+  Cell cell1;
+  Cell cell2;
+
+  public LessThanFormula(Cell cell1, Cell cell2) {
+    if (!cell1.isNumericValue() | !cell2.isNumericValue()) {
+      throw new IllegalArgumentException();
+    }
+    this.cell1 = cell1;
+    this.cell2 = cell2;
+  }
+
+  public LessThanFormula() {
+    this.cell1 = null;
+    this.cell2 = null;
+  }
+
+  @Override
+  public Boolean evaluate() {
+    // casted because at this point we know it is a double
+    return (double) cell1.getValue() < (double) cell2.getValue();
+  }
+
+  @Override
+  public void addFormula(Formula form) {
+    throw new IllegalCallerException("LessThanFormula doesn't contain formula");
+  }
+
+  @Override
+  public void addCell(Cell cell) {
+    throw new IllegalCallerException("LessThanFormula has to be intialized with cells");
+  }
+
+}
